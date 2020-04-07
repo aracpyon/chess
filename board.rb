a5e433010c90fcd23e3c494ddb0148c20866250d
Ara Cho <artistaracho@gmail.com>
2020-04-07 00:12:03 -0400

    update chess

--- board.rb
+++ board.rb
@@ -14,4 +14,21 @@ class Board
     x, y = pos
     @rows[x][y] = val
   end
+
+  def move_piece(color, start_pos, end_pos)
+    raise "there is nothing here" if self[start_pos] == nil
+
+    piece = self[start_pos]
+    if peice.color != color
+      
+    end
+
+    self[end_pos] = piece
+    #
+    piece.pos = end_pos
+  end
+
+  def move_piece!(start_pos, end_pos)
+
+  end
 end
